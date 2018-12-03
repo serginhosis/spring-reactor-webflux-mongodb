fb2244c03f1dac1e6d717b86a2aeceb96340dafe
snascimento <snascimento@AC-LP0039-snascimento>
2018-12-03 15:07:26 -0200

    Add more useful files

---
+++ src/main/java/br/com/sis/webflux/springreactorwebfluxmongodb/SpringReactorWebfluxMongodbApplication.java
@@ -0,0 +1,12 @@
+package br.com.sis.webflux.springreactorwebfluxmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactorWebfluxMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactorWebfluxMongodbApplication.class, args);
+	}
+}
